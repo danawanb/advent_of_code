b45ad4a1ee1bc6ee6761890144f99402e280f99f
danawan <danawanbimantoro23@gmail.com>
2024-12-03 14:15:26 +0700

    add day2 chapter 2

--- src/day2.rs
+++ src/day2.rs
@@ -15,6 +15,86 @@ pub fn day_two() -> i32 {
             input.push(valx);
         }
     }
+
+    //let resx = count_zero(safety_count);
+    //println!("{}", resx);
+
+    let mut count = 0;
+    for i in input {
+        let mut res_map = checksafety(&i);
+
+        if res_map == false {
+            let mut child_safe;
+
+            for x in 0..i.len() {
+                let mut temp = i.clone();
+                temp.remove(x);
+                //println!("{:?}", temp); // Cetak hasil
+
+                child_safe = checksafety(&temp);
+                if child_safe == true {
+                    res_map = true;
+                    break;
+                }
+                //println!("{:?} {}", temp, res_map); // Cetak hasil
+            }
+        }
+        println!("{:?} {}", i, res_map);
+        if res_map == true {
+            count += 1
+        }
+    }
+
+    println!("{}", count);
+    return count;
+}
+
+fn checksafety(input: &Vec<i32>) -> bool {
+    let mut unsafety: bool = true;
+
+    let mut has_increase = false;
+    let mut has_decrease = false;
+
+    for x in input.windows(2) {
+        let count: i32 = x[1] - x[0];
+
+        //if increase or decrease more than 3 || neither an increase or decrease
+        if count.abs() > 3 || count.abs() == 0 {
+            unsafety = false;
+            break;
+        }
+
+        //increase
+        if count > 0 {
+            has_increase = true
+        } else if count < 0 {
+            has_decrease = true
+        }
+
+        if has_increase && has_decrease {
+            unsafety = false;
+            break;
+        }
+    }
+
+    return unsafety;
+}
+
+fn day_two_part1() -> i32 {
+    let txt = fs::read_to_string("./src/day2.txt");
+    //let txt = fs::read_to_string("./src/day2_dummy.txt");
+
+    let mut input: Vec<Vec<i32>> = Vec::new();
+
+    if let Ok(val) = txt {
+        for (_, s_val) in val.lines().enumerate() {
+            let valx: Vec<i32> = s_val
+                .split_whitespace()
+                .map(|s| s.parse().unwrap())
+                .collect();
+            input.push(valx);
+        }
+    }
     let mut safex: HashMap<Vec<i32>, i32> = HashMap::new();
     let mut safety_count: Vec<i32> = Vec::new();
     for i in input {
