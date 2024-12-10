18fe10e6fbe970468cc20194907c2a2979f98c28
danawan <danawanbimantoro23@gmail.com>
2024-12-10 16:16:29 +0700

    day 8 chapter 2

--- src/day8.rs
+++ src/day8.rs
@@ -1,4 +1,4 @@
-use std::fs;
+use std::{fs, isize};
 
 pub fn day_eight() -> i32 {
     let txt = fs::read_to_string("./src/day8.txt");
@@ -80,9 +80,43 @@ pub fn day_eight() -> i32 {
         }
     }
 
-    println!("{}", catchers.len());
+    //println!("{}", catchers.len());
 
-    catchers.len() as i32
+    let mut catchersx = Vec::new();
+
+    let i_len = input.lines().count() as i32;
+    let j_len = input.lines().nth(0).unwrap().chars().count() as i32;
+
+    for (i, pos) in antena.iter().enumerate() {
+        for (j, pos2) in antena.iter().enumerate() {
+            if pos.value == pos2.value && i != j {
+                if !catchersx.contains(&(pos.x as isize, pos.y as isize)) {
+                    catchersx.push((pos.x as isize, pos.y as isize));
+                }
+
+                // offsetnya
+                let pos_i_diff = pos.x - pos2.x;
+                let pos_j_diff = pos.y - pos2.y;
+
+                let mut point_location = (pos_i_diff + pos.x, pos_j_diff + pos.y);
+
+                while point_location.0 >= 0
+                    && point_location.1 >= 0
+                    && point_location.0 < i_len
+                    && point_location.1 < j_len
+                {
+                    if !catchersx.contains(&(point_location.0 as isize, point_location.1 as isize))
+                    {
+                        catchersx.push((point_location.0 as isize, point_location.1 as isize));
+                    }
+                    point_location = (pos_i_diff + point_location.0, pos_j_diff + point_location.1);
+                }
+            }
+        }
+    }
+
+    let res = catchersx.len() as i32;
+    res
 }
 
 #[derive(Debug)]
