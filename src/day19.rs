a5278179692c64edd5803aa7a26ea7e4733b2b9d
danawan <danawanbimantoro23@gmail.com>
2024-12-23 16:22:16 +0700

    day 19 chapter 2

--- src/day19.rs
+++ src/day19.rs
@@ -24,13 +24,20 @@ pub fn day_nineteen() -> usize {
 
     let mut countx = 0;
 
+    //for design in &onsen_display {
+    //if can_construct_design(&towel, design) {
+    //countx += 1;
+    //}
+    //}
+
+    let mut countx_part2 = 0;
+
     for design in &onsen_display {
-        if can_construct_design(&towel, design) {
-            countx += 1;
-        }
+        let cw = cw_to_construct(&towel, design);
+        countx_part2 += cw;
     }
 
-    countx
+    countx_part2
 }
 
 fn can_construct_design(patterns: &Vec<String>, design: &str) -> bool {
@@ -63,4 +70,31 @@ fn can_construct(target: String, patterns: &Vec<String>, memo: &mut HashMap<Stri
     //insert ke memo sementara
     memo.insert(target.clone(), false);
     return false;
+}
+
+fn cw_to_construct(patterns: &Vec<String>, design: &str) -> usize {
+    let mut memo: HashMap<String, usize> = HashMap::new();
+    return cw(design.to_string(), patterns, &mut memo);
+}
+
+fn cw(target: String, patterns: &Vec<String>, memo: &mut HashMap<String, usize>) -> usize {
+    if memo.contains_key(&target) {
+        return *memo.get(&target).unwrap();
+    }
+
+    if target.is_empty() {
+        return 1;
+    }
+
+    let mut total_ways = 0;
+
+    for pattern in patterns {
+        if target.starts_with(pattern) {
+            let suffix = target[pattern.len()..].to_string();
+            total_ways += cw(suffix, patterns, memo);
+        }
+    }
+
+    memo.insert(target.clone(), total_ways);
+    return total_ways;
 }
