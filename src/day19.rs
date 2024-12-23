1a8799e9bcf630476c93ccc6a7e5001d211aa66c
danawan <danawanbimantoro23@gmail.com>
2024-12-23 16:13:41 +0700

    day 19 chapter 1

---
+++ src/day19.rs
@@ -0,0 +1,66 @@
+use std::{collections::HashMap, fs};
+
+pub fn day_nineteen() -> usize {
+    let txt = fs::read_to_string("./src/day19.txt");
+    let mut txt_str: Vec<String> = Vec::new();
+    if let Ok(x) = txt {
+        txt_str = x.split("\n\n").map(|f| f.to_string()).collect();
+    }
+
+    let mut towel: Vec<String> = Vec::new();
+    if let Some(xx) = txt_str.get(0) {
+        towel = xx
+            .split(",")
+            .map(|f| f.trim().to_string()) // Hilangkan whitespace di awal dan akhir string
+            .filter(|f| !f.is_empty())
+            .collect();
+    }
+
+    //towel to display
+    let mut onsen_display: Vec<String> = Vec::new();
+    if let Some(xx) = txt_str.get(1) {
+        onsen_display = xx.lines().map(|x| x.to_string()).collect();
+    }
+
+    let mut countx = 0;
+
+    for design in &onsen_display {
+        if can_construct_design(&towel, design) {
+            countx += 1;
+        }
+    }
+
+    countx
+}
+
+fn can_construct_design(patterns: &Vec<String>, design: &str) -> bool {
+    let mut memo: HashMap<String, bool> = HashMap::new();
+    return can_construct(design.to_string(), patterns, &mut memo);
+}
+
+fn can_construct(target: String, patterns: &Vec<String>, memo: &mut HashMap<String, bool>) -> bool {
+    //cek key jika ada ambil valuenya
+    if memo.contains_key(&target) {
+        return *memo.get(&target).unwrap();
+    }
+
+    //cek apakah sudah targetnya ada
+    if target.is_empty() {
+        return true;
+    }
+
+    //looping patternnya
+    for pattern in patterns {
+        if target.starts_with(pattern) {
+            let suffix = target[pattern.len()..].to_string();
+            if can_construct(suffix, patterns, memo) {
+                memo.insert(target.clone(), true);
+                return true;
+            }
+        }
+    }
+
+    //insert ke memo sementara
+    memo.insert(target.clone(), false);
+    return false;
+}
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod day15;
 mod day16;
 mod day17;
 mod day18;
+mod day19;
 mod day2;
 mod day3;
 mod day4;
@@ -86,4 +87,7 @@ fn main() {
 
     //let eighteen = day18::day_eighteen();
     //println!("{}", eighteen);
+
+    let nineth = day19::day_nineteen();
+    println!("{}", nineth);
 }
